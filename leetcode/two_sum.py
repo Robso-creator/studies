a6a8b04d8d4a67661a5cf5ce6a507696087dfff3
Robson Allef - Windows <contato.robsonallef@gmail.com>
2023-12-13 21:24:26 -0300

    first commit

---
+++ leetcode/two_sum.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/two-sum/
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            
+
+if __name__ == "__main__":
+    tt =  twoSum([3,3], 6)
+    breakpoint()
